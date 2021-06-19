674abdf6700439f0c895315d5edbdeef9290c559
DavidTibet <vodopyanovdaniil@gmail.com>
2021-06-19 20:46:12 +0300

    Test

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 variable "server_port" {
-  description = "The port the server will use for HTTP requests"
+  description = "Port that server will use for HTTP requests"
   type        = number
   default     = 8080
 }
